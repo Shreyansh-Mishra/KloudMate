0d0e5f3f642959ae75db201538adebf5896f7af3
Shreyansh-Mishra <201951146@iiitvadodara.ac.in>
2022-04-01 19:40:47 +0530

    Made a Todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Tasks from "./Tasks";
+import TodoInput from "./TodoInput";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://todoapphasura.herokuapp.com/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Todo App</p>
+        </header>
+        <br />
+        <TodoInput />
+        <Tasks />
+      </div>
+    </ApolloProvider>
   );
 }
 
